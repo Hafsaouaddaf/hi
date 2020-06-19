3d52489b7001ff1ebbac8ff2ceeaddf5d1b7b19b
Hafsaouaddaf <65956992+Hafsaouaddaf@users.noreply.github.com>
2020-06-19 03:31:52 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        strcon = "Data Source=DESKTOP-CCQJNK1;Initial Catalog=H_I ;Integrated Security=True"
+        comhi = New SqlCommand(str)
+        conhi = New SqlConnection
+        conhi.ConnectionString = strcon
+        conhi.Open()
+        If conhi.State = ConnectionState.Open Then
+            MsgBox("Vous  êtes  connectés", 0 + 64, "connecté")
+        End If
+        odahi = New SqlDataAdapter(comhi)
+        comhi.Connection = conhi
+    End Sub
+End Class
---
+++ Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+Module Module1
+    Public conhi As New SqlConnection
+    Public comhi As New SqlCommand
+    Public odahi As New SqlDataAdapter
+    Public odrhi As SqlDataReader
+    Public odshi As New DataSet
+    Public str As String
+    Public strcon As String
+    Public builderhi As SqlCommandBuilder
+End Module
